ed1141160d3aae26fc2c2ff0f63f34bef5765342
SAWYER SAWYER <mehaleka240399@yandex.ru>
2025-08-19 21:40:21 +0300

    docs: добавлена типизация(исправлено) и начата документация

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,33 +1,43 @@
-export interface ICardsPreview { //набор карточек на главной странице
+export interface ICard { //карточка
   _id: string;
   image: string;
   title: string;
   category: string;
   price: number | null;
+  description?: string;
 }
 
-export interface ICard { //сама карточка товара когда мы на нее нажимаем и хотим посмотреть 
-  cards: ICardsPreview[];
-  description: string;
-  preview: string | null; //превью карточки, которую хотим посмотреть
-}
-
-export interface IValidationForm { //валидация форм
-  error: string;
-}
+export interface ICardData { //структура, которая отвечает за множество карточек 
+  cards: ICard[];
+  preview: string | null; 
+  openCard(card: ICard): void;
+  closeCard(card: ICard): void;
+  getCard(cardID: string): ICard;
+  buyCard(card: ICard): void; 
+  deleteCard(cardID: string): void; 
+} 
 
 export interface IBasket { //корзина 
   _id: string;
   total: number | null;
+  items: ICard[]; 
+  addItem(card: ICard): void; 
+  removeItem(cardId: string): void; 
+  clear(): void; 
+  getItems(): ICard[]; 
 }
 
-export interface ICardPreview { //набор карточек на главной странице
-  _id: string;
-  image: string;
-  title: string;
-  category: string;
-  price: number | null;
+
+export interface IValidationForm {
+  buttonError: boolean; //не выбрана кнопка
+  addressError: string; //не введен адрес доставки
+  emailError: string; //неверный email
+  phoneError: string; //неверный номер телефона
+  checkValidation(): boolean; // Метод для проверки всех полей
 }
+
+
+
 
 // пример типа, которые могут использовать
 // уже прописанные интерфейсы T... = Pick<ICard, '_id' | 'category' | 'title' | 'image' | 'price'>; //главная страница
